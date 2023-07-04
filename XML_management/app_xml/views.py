6d0256010c35fbdef6abe8b1f05de7bf9c8a3b28
Harshtroo <123172610+Harshtroo@users.noreply.github.com>
2023-07-04 23:11:56 +0530

    done edit

--- XML_management/app_xml/views.py
+++ XML_management/app_xml/views.py
@@ -1,13 +1,16 @@
 import xml.etree.ElementTree as ET
 from django.shortcuts import render, redirect
-from django.views.generic import UpdateView,TemplateView,FormView
+from django.views.generic import UpdateView,TemplateView,FormView,View
 import xmltodict
+from django.conf import settings
 from django.urls import reverse
+from datetime import datetime
+
 
 
 
 def show_xml_file(request):
-    with open("/home/ashishv/XML_project/XML_management/app_xml/user_data.xml","r", encoding="utf-8") as file:
+    with open(settings.XML_FILE_PATH,"r", encoding="utf-8") as file:
         user_xml = file.read()
     user_dict = xmltodict.parse(user_xml)
     user_data = user_dict["employees"]
@@ -17,12 +20,11 @@ def show_xml_file(request):
     return render(request,"user_list.html", {"user_data" : user_data["employee"]})
 
 
-class EmployeeEdit(FormView):
+class EmployeeEdit(View):
     template_name = "employee_update.html"
-    # success_url = reverse("show_xml")
 
     def get(self, request, *args, **kwargs):
-        with open("/home/ashishv/XML_project/XML_management/app_xml/user_data.xml", "r", encoding="utf-8") as file:
+        with open(settings.XML_FILE_PATH, "r", encoding="utf-8") as file:
             user_xml = file.read()
         user_dict = xmltodict.parse(user_xml)
         user_data = user_dict["employees"]
@@ -39,27 +41,24 @@ class EmployeeEdit(FormView):
         return render(request,self.template_name,{"select_employee_data": select_employee_data})
 
     def post(self, request, *args, **kwargs):
-        print("post_data=============",request.POST)
+        tree = ET.parse('app_xml/static/xml/user_data.xml')
+        root = tree.getroot()
+        user_id = request.POST["id"]
+        employee = root.find(f"./employee[@id='{user_id}']")
 
-        employees = ET.Element("employees")
-        employee = ET.SubElement(employees,"employee",id=request.POST["id"])
+        if employee is not None:
+            # Update the desired data
+            employee.find('firstname').text = request.POST["firstname"]
+            employee.find('lastname').text = request.POST["lastname"]
+            employee.find('title').text = request.POST["title"]
+            employee.find('division').text = request.POST["division"]
+            employee.find('building').text = request.POST["building"]
+            employee.find('room').text =  request.POST["room"]
 
-        firstname = ET.SubElement(employee,'firstname')
-        lastname = ET.SubElement(employee,"lastname")
-        title = ET.SubElement(employee,"title")
-        division = ET.SubElement(employee,"division")
-        building = ET.SubElement(employee,"building")
-        room = ET.SubElement(employee,"room")
-
-        firstname.text = request.POST["firstname"]
-        lastname.text = request.POST["lastname"]
-        title.text = request.POST["title"]
-        division.text = request.POST["division"]
-        building.text = request.POST["building"]
-        room.text = request.POST["room"]
-
-        tree = ET.ElementTree(employees)
-
-        tree.write(f"user_data.xml")
+            # Save the updated XML back to the file
+            current_time = datetime.now().strftime("%Y_%m_%d-%I-%M-%p")
+            file_path = f'app_xml/static/xml/user_data-{current_time}.xml'
+            tree.write(file_path)
+            settings.XML_FILE_PATH = file_path
 
         return redirect("/")
